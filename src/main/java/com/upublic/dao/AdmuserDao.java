1915f142ff18c2b342ddd7f1433ea2a300e3b849
张悦 <1136811207@qq.com>
2017-05-07 00:00:31 +0800

    增加后台用户表、搜素表和用户表的实体映射。

---
+++ src/main/java/com/upublic/dao/AdmuserDao.java
@@ -0,0 +1,10 @@
+package com.upublic.dao;
+
+import com.upublic.vo.Admuser;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by Administrator on 2017/5/6.
+ */
+public interface AdmuserDao {
+}
