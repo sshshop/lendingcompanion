bcd9004626b3fe46ccefa0dde5d4813782e47f4b
Freewill-run <780320398@qq.com>
2017-05-06 22:53:58 +0800

    省份和城市的映射

---
+++ src/main/java/com/upublic/dao/cityDao.java
@@ -0,0 +1,14 @@
+package com.upublic.dao;
+
+import com.upublic.vo.City;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jidy on 2017/5/6.
+ */
+public class cityDao {
+
+}
