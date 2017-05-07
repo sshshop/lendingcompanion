cd5edf0a9f617478cebe21ea0c67861bd657d854
Freewill--run <780320398@QQ.COM>
2017-05-07 10:19:26 +0800

    修改省份和城市映射的多余部分

--- src/main/java/com/upublic/dao/ProvinceDao.java
+++ src/main/java/com/upublic/dao/ProvinceDao.java
@@ -3,5 +3,5 @@ package com.upublic.dao;
 /**
  * Created by jidy on 2017/5/6.
  */
-public class ProvinceDao {
+public interface ProvinceDao {
 }
--- src/main/java/com/upublic/dao/cityDao.java
+++ src/main/java/com/upublic/dao/cityDao.java
@@ -1,14 +1,9 @@
 package com.upublic.dao;
 
-import com.upublic.vo.City;
-import org.apache.ibatis.session.SqlSession;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by jidy on 2017/5/6.
  */
-public class cityDao {
+public interface cityDao {
 
 }
