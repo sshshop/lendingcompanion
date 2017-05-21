ab89839965e87a77a8d11a927bbd1ed372cdde95
Rabit <lj79490324@gmail.com>
2017-05-21 17:43:10 +0800

    同一本书不能借阅两次bug修改完

--- src/main/java/com/lj/borrowbook/service/BorrowbookService.java
+++ src/main/java/com/lj/borrowbook/service/BorrowbookService.java
@@ -15,6 +15,11 @@ public class BorrowbookService {
     @Resource(name = "borrowbookDao")
     private BorrowbookDao borrowbookDao;
 
+    /**
+     * 插入借书信息
+     * @param user 用户id
+     * @param borrowbook 图书id
+     */
     public void insertBorrowdbook(User user, Borrowbook borrowbook) {
         borrowbook.setUid(user.getUid());
         borrowbookDao.insertBorrowdbook(borrowbook);
@@ -22,7 +27,6 @@ public class BorrowbookService {
 
     /**
      * 当用户借书年大于2等于2时。返回一个对应的bool值
-     *
      * @param user 用户id封装的实体类
      * @return 返回布尔值，当数字大于等于2返回false，反之返回true
      */
@@ -30,8 +34,22 @@ public class BorrowbookService {
         return borrowbookDao.selectCountBorrowBook(user) >= 2 ? false : true;
     }
 
+    /**
+     * 更新书籍的借阅信息
+     * @param borrowbook 提供书籍的id
+     */
     public void updateBookNborrow(Borrowbook borrowbook) {
         borrowbookDao.updateBookNborrow(borrowbook);
+    }
+
+    /**
+     * 查询同一本书用户是否是二次借阅
+     * @param user 用户id封装
+     * @param borrowbook 图书id封装
+     * @return 返回存在返回false，不存在返回true
+     */
+    public boolean existBorrMsg(User user,Borrowbook borrowbook){
+            return borrowbookDao.existBorrMsg(user,borrowbook)>=1?false:true;
     }
 
 }
